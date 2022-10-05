4f148d46d27142ab3a89405915ab04415ede56d9
abdshami <abdshami8888@gmail.com>
2022-10-05 22:53:40 +0300

    test

---
+++ BookingSecond/src/test/java/Core/Testkh.java
@@ -0,0 +1,57 @@
+package Core;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+
+public class Testkh {
+
+	WebDriver driver;
+	Add add;
+	 
+
+	@BeforeSuite
+		public void beforeTest() throws InterruptedException  {
+			  add = new Add();   
+		      System.out.println("browser"+ "   khaldoun Ok" );  
+			  String browser = "chrome";
+		      if (browser.equals("chrome")) {
+					WebDriverManager.chromedriver().setup();
+					driver = new ChromeDriver();
+				}else if (browser.equals("firefox")) {
+					WebDriverManager.firefoxdriver().setup();
+					driver = new FirefoxDriver();
+				}else if (browser.equals("edge")) {
+					WebDriverManager.edgedriver();
+					driver = new EdgeDriver();
+				}	
+			 
+		      System.out.println("After browser 1" );
+			  driver.manage().window().maximize();
+			  driver.get("https://www.google.com");
+			  System.out.println("After browser 2" );
+			    
+		}
+		
+	    @Test
+	    public void testMethod(){
+	        System.out.println("yes" );
+	        add.addNums(5, 9);
+			Assert.assertEquals(2, 2);
+		}
+	
+	   
+	
+	
+
+}
