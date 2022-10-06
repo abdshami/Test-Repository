6d26e8bdb53ef82679218e7f74db22274f8551b0
abdshami <abdshami8888@gmail.com>
2022-10-06 10:48:41 +0300

    commit test

--- BookingForMe/BookingSecond/src/test/java/Core/Testkh.java
+++ BookingForMe/BookingSecond/src/test/java/Core/Testkh.java
@@ -21,27 +21,26 @@ public class Testkh {
 	 
 
 	@BeforeSuite
-		public void beforeTest() throws InterruptedException  {
+		public void beforeSuite() throws InterruptedException  {
 			  add = new Add();   
-//		      System.out.println("browser"+ "   khaldoun Ok" );  
-//			 
+//		      System.out.println("browser"+ "   khaldoun Ok" );  		 
 //		           System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
 //				   driver = new ChromeDriver();
 //		      
-////					WebDriverManager.chromedriver().setup();
-////					driver = new ChromeDriver();
-//				
-		      System.out.println("After browser 1" );
+//					//WebDriverManager.chromedriver().setup();
+//					//driver = new ChromeDriver();
 //			  driver.manage().window().maximize();
 //			  driver.get("https://www.google.com");
-			  System.out.println("After browser 2" );
-			    
+//				
+		      System.out.println("beforeSuite" );
+			
+			
 		}
 		
 	    @Test
 	    public void testMethod(){
-	        System.out.println("yes" );
 	        add.addNums(5, 9);
+	        System.out.println("Assert Test Method" );
 			Assert.assertEquals(2, 2);
 		}
 	
