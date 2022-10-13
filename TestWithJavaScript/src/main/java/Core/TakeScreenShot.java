d7f3f0121839615ee0196f192fa4254ad2e53895
Your Name <abdshami8888@gmail.com>
2022-10-13 23:38:11 +0300

    Part of Booking Task Updating no 7

--- TestWithJavaScript/src/main/java/Core/TakeScreenShot.java
+++ TestWithJavaScript/src/main/java/Core/TakeScreenShot.java
@@ -2,11 +2,17 @@ package Core;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+
+import io.qameta.allure.Allure;
 
 public class TakeScreenShot {
 	
@@ -21,6 +27,14 @@ public class TakeScreenShot {
 		File SrcFile=scrShot.getScreenshotAs(OutputType.FILE);
 		File DestFile=new File(filePath);
 		FileUtils.copyFile(SrcFile, DestFile);
+		Path content = Paths.get(filePath);
+		try (InputStream is = Files.newInputStream(content)){
+			Allure.addAttachment("HotelImage", is);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+				
+		
 	}
 }
 
