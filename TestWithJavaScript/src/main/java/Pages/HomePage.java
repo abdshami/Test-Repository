67ccbc0732f3e0182ef26531cf96581ecfdbf8c6
Your Name <abdshami8888@gmail.com>
2022-10-16 23:46:40 +0300

    Part of Booking Task Updating no 10

--- TestWithJavaScript/src/main/java/Pages/HomePage.java
+++ TestWithJavaScript/src/main/java/Pages/HomePage.java
@@ -13,8 +13,7 @@ public class HomePage {
 
 	WebDriver driver;
 	JavascriptExecutor js;
-	
-	
+
 	public HomePage(WebDriver driver, JavascriptExecutor js) {
 		this.driver = driver;
 		this.js = js;
@@ -39,59 +38,20 @@ public class HomePage {
 				Thread.sleep(4000);
 				WebElement ChkIn  = driver.findElement(By.xpath("//div[@data-testid = 'date-start-input']"));
 				WebElement ChkOut  = driver.findElement(By.xpath("//div[@data-testid = 'date-end-input']"));
-	
-//				//div[@class='Input_control__es8pY Input_control__es8pY']
-//				List<WebElement> dates  = driver.findElements(By.xpath("//div[@class = 'Input_control__es8pY Input_control__es8pY']"));
-				js.executeScript(" return arguments[0].click();", ChkIn);
-				Thread.sleep(4000);
-				
-				
-//				WebElement date1  = driver.findElement(By.xpath("//div[@class = 'Input_label__ay4At Input_label_size_m__JeQk3 Input_label_focused__EmPIw']"));
-//				js.executeScript("arguments[0].click() ;", date1);
-//				System.out.println("date1 click");
-//				Thread.sleep(2000);
-				
-				List<WebElement> years  = driver.findElements(By.xpath("//div[@class = 'Month_year__2Uuda']"));
-				js.executeScript("arguments[0].click() ;", years.get(0));
-				System.out.println("year click");
-				Thread.sleep(7000);
-				
-				List<WebElement> months  = driver.findElements(By.xpath("//div[@class = 'Month_title__1PKGU']"));
-				for(WebElement we : months) {
-					String text = (String) js.executeScript(" return arguments[0].innerText;", we);
-					if(text.equals("May")) {
-						js.executeScript(" return arguments[0].click();", we);
-					}
-				}
-				System.out.println("month click");
-				Thread.sleep(5000);
-				
-				
-				
-				
-				//js.executeScript("document.getElementsByClassName('sb-date-field__field')[0].click();");
-				//js.executeScript("document.getElementsByClassName('sb-date-field__field')[0].click();");
-				
-			 //   js.executeScript("document.getElementsByClassName('sb-date-field__field')[0].click();");
-			 //   driver.findElement(By.xpath("//div[@data-testid = 'date-start-input']"));
-			//	WebElement ChkOut1  = driver.findElement(By.xpath("//div[@data-testid = 'date-end-input']"));
-
-				
-				
-//				Thread.sleep(2000);
-//				js.executeScript("arguments[0].click();", ChkIn);
-//				Thread.sleep(2000);
-//				js.executeScript("arguments[0].innerText = '" + chkIn + "';", ChkIn);
-//				Thread.sleep(2000);
-//				js.executeScript("arguments[0].click();", ChkOut);
-//				Thread.sleep(2000);
-//				js.executeScript("arguments[0].innerText = '" + chkOut + "';", ChkOut);
-//				Thread.sleep(2000);
+				Thread.sleep(2000);
+				js.executeScript("arguments[0].click();", ChkIn);
+				Thread.sleep(2000);
+				js.executeScript("arguments[0].innerText = '" + chkIn + "';", ChkIn);
+				Thread.sleep(2000);
+				js.executeScript("arguments[0].click();", ChkOut);
+				Thread.sleep(2000);
+				js.executeScript("arguments[0].innerText = '" + chkOut + "';", ChkOut);
+				Thread.sleep(2000);
 		}else {
-			//System.out.println("There is no results");
+			System.out.println("There is no results");
 		}
 		WebElement searchBtn = driver.findElement(By.xpath("//button[@data-testid='search-button']"));
-		//js.executeScript("arguments[0].click();", searchBtn);
+		js.executeScript("arguments[0].click();", searchBtn);
 		Thread.sleep(10000);
 	}
 }
